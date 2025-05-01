d95454ddb1b28af7590c23835296d4c9929129b6
davigama10 <davigama10@gmail.com>
2025-05-01 12:43:57 -0300

    progredindo

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
     DB_URL: str = "postgresql+asyncpg://postgres:84265@localhost:5432/faculdade"
-    DBBaseModel = declarative_base()
+    DBBaseModel: type = declarative_base()
 
     JWT_SECRET: str = 'POL5qNXBy3KpGR9_qLKEKLZ6wgyQJ5y6KP8TI1Vpxa8'
     """
